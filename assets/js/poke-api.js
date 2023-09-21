424c120f60d98f9f715968b9849a9eca88188b79
MateusMarquessz <mateusmarques53@outlook.com>
2023-09-21 15:36:26 -0300

    Update poke-api.js

--- assets/js/poke-api.js
+++ assets/js/poke-api.js
@@ -1,7 +1,7 @@
- const pokeApi = {}
 
+const pokeApi = {}
 
-function convertPokeApiDetailToPokemon(pokeDetail){
+function convertPokeApiDetailToPokemon(pokeDetail) {
     const pokemon = new Pokemon()
     pokemon.number = pokeDetail.id
     pokemon.name = pokeDetail.name
@@ -17,20 +17,19 @@ function convertPokeApiDetailToPokemon(pokeDetail){
     return pokemon
 }
 
-pokeApi.getPokemonDetail = (pokemon) =>{
+pokeApi.getPokemonDetail = (pokemon) => {
     return fetch(pokemon.url)
-    .then((response) => response.json())
-    .then((convertPokeApiDetailToPokemon))
+        .then((response) => response.json())
+        .then(convertPokeApiDetailToPokemon)
 }
 
- pokeApi.getPokemons = (offset = 0, limit = 248) =>{
-        const url = 'https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}'
+pokeApi.getPokemons = (offset = 0, limit = 5) => {
+    const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
 
-        return fetch(url)
-            .then((response) => response.json()) 
-            .then((jsonBody) => jsonBody.results)
-            .then((pokemon) => pokemon.map(pokeApi.getPokemonDetail))
-            .then((detailRequests) => Promise.all(detailRequests))
-            .then((pokemonsDetails) => pokemonsDetails)
-            .catch((error) => console.error(error))
+    return fetch(url)
+        .then((response) => response.json())
+        .then((jsonBody) => jsonBody.results)
+        .then((pokemons) => pokemons.map(pokeApi.getPokemonDetail))
+        .then((detailRequests) => Promise.all(detailRequests))
+        .then((pokemonsDetails) => pokemonsDetails)
 }
